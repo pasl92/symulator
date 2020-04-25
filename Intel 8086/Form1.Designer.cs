677efd7b69264ff3af8dda0ca59d1625fdcc6709
pasl92 <pawel.salata@microsoft.wsei.edu.pl>
2020-04-25 11:40:53 +0200

    run app

--- Intel 8086/Form1.Designer.cs
+++ Intel 8086/Form1.Designer.cs
@@ -43,6 +43,8 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -68,34 +70,43 @@
             // 
             // textBox1
             // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox1.Location = new System.Drawing.Point(75, 102);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(160, 20);
             this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "1111";
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox2.Location = new System.Drawing.Point(75, 142);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(160, 20);
             this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "2222";
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // textBox3
             // 
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox3.Location = new System.Drawing.Point(75, 183);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(160, 20);
             this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "3333";
             this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // textBox4
             // 
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox4.Location = new System.Drawing.Point(75, 221);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(160, 20);
             this.textBox4.TabIndex = 5;
+            this.textBox4.Text = "4444";
             this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
             // 
             // comboBox1
@@ -109,7 +120,7 @@
             "Dx"});
             this.comboBox1.Location = new System.Drawing.Point(75, 290);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(134, 21);
+            this.comboBox1.Size = new System.Drawing.Size(159, 21);
             this.comboBox1.TabIndex = 6;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
@@ -124,7 +135,7 @@
             "Dx"});
             this.comboBox2.Location = new System.Drawing.Point(293, 290);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(155, 21);
+            this.comboBox2.Size = new System.Drawing.Size(179, 21);
             this.comboBox2.TabIndex = 7;
             this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
@@ -168,7 +179,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(240, 293);
+            this.label5.Location = new System.Drawing.Point(254, 293);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(19, 13);
             this.label5.TabIndex = 12;
@@ -185,12 +196,34 @@
             this.pictureBox1.TabIndex = 13;
             this.pictureBox1.TabStop = false;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(293, 36);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(76, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Info";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(396, 36);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(76, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "O mnie";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Info;
             this.ClientSize = new System.Drawing.Size(542, 349);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -219,7 +252,6 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox4;
@@ -231,6 +263,9 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        public System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Intel 8086/Form1.cs
+++ Intel 8086/Form1.cs
@@ -12,11 +12,11 @@ namespace Intel_8086
 {
     public partial class Form1 : Form
     {
-
-        string rax;
-        string rbx;
-        string rcx;
-        string rdx;
+        private string rax;
+        private string rbx;
+        private string rcx;
+        private string rdx;
+        private string x;
 
 
         public Form1()
@@ -54,26 +54,60 @@ namespace Intel_8086
         private void button2_Click(object sender, EventArgs e)
         {
 
-            string x = comboBox1.Items[0].ToString();
-
-
             if (comboBox1.SelectedIndex == comboBox2.SelectedIndex)
             {
                 MessageBox.Show("Wybierz poprawny rejestr");
             }
 
-            if ((comboBox1.SelectedIndex == 1) && (comboBox2.SelectedIndex == 2))
-            {
+            //if ((comboBox1.SelectedIndex == 0) && (comboBox2.SelectedIndex == 1))
+            //{
+            //    textBox1.Text = textBox2.Text;
+            //}
 
+            if (comboBox2.SelectedIndex == 0)
+            {
+                textBox1.Text = x;
             }
 
+            if (comboBox2.SelectedIndex == 1)
+            {
+                textBox2.Text = x;
+            }
+
+            if (comboBox2.SelectedIndex == 2)
+            {
+                textBox3.Text = x;
+            }
+
+            if (comboBox2.SelectedIndex == 3)
+            {
+                textBox4.Text = x;
+            }
         }
 
        
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                x = textBox1.Text.ToString();
+            }
 
+            if (comboBox1.SelectedIndex == 1)
+            {
+                x = textBox2.Text.ToString();
+            }
+
+            if (comboBox1.SelectedIndex == 2)
+            {
+                x = textBox3.Text.ToString();
+            }
+
+            if (comboBox1.SelectedIndex == 3)
+            {
+                x = textBox4.Text.ToString();
+            }
         }
 
         private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
@@ -84,22 +118,32 @@ namespace Intel_8086
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            string rax = textBox1.Text;
+            rax = textBox1.Text;
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
-            string rbx = textBox2.Text;
+            rbx = textBox2.Text;
         }
 
         private void textBox3_TextChanged(object sender, EventArgs e)
         {
-            string rcx = textBox3.Text;
+            rcx = textBox3.Text;
         }
 
         private void textBox4_TextChanged(object sender, EventArgs e)
         {
-            string rdx = textBox4.Text;
+            rdx = textBox4.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("8086 – 16-bitowy mikroprocesor wprowadzony na rynek 8 czerwca 1978 roku");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Paweł Sałata Informatyka i Ekonomteria 2019/2020");
         }
     }
 }
